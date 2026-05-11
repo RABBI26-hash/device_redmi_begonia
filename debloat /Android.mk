295ff3c879069baf26749c534ea4d2af0801d0bf
Rabbi Raihan <rbrabbi789@gmail.com>
2026-05-11 15:05:07 +0600

    Add some unwanted packages to debloat

--- debloat /Android.mk
+++ debloat /Android.mk
@@ -1,1 +1,22 @@
-
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := Debloat
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES += AmbientSensePrebuilt AppDirectedSMSService arcore DevicePolicyPrebuilt-v10334460 SwitchAccessPrebuilt_1.16.0.726766860 AndroidAutoStubPrebuilt 
+LOCAL_OVERRIDES_PACKAGES += CarrierSetup ConnMO ConnMetrics GoogleRestorePrebuilt-v842048 ScribePrebuilt_v8.4.773573318 TurboPrebuilt MatLog DevicePersonalizationPrebuiltPixel2025-playstore_aiai_20250306.00_RC10
+LOCAL_OVERRIDES_PACKAGES += DCMO  DiagnosticsToolPrebuilt DMService
+LOCAL_OVERRIDES_PACKAGES += GCS GoogleCamera GoogleTTS Jelly
+LOCAL_OVERRIDES_PACKAGES += MaestroPrebuilt Maps MyVerizonServices NovaBugReportWrapper Ornament OemDmTrigger OBDM_Perm obdm_stub
+LOCAL_OVERRIDES_PACKAGES += Phonograph PixelLiveWallpaperPrebuilt
+LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SCONE ScribePrebuilt Showcase SimpleCalculator SimpleCalendar Snap SoundAmplifierPrebuilt SprintDM SprintHM
+LOCAL_OVERRIDES_PACKAGES += Tag Tycho
+LOCAL_OVERRIDES_PACKAGES += USCCDM
+LOCAL_OVERRIDES_PACKAGES += Videos VZWAPNLib VzwOmaTrigger
+LOCAL_OVERRIDES_PACKAGES += WallpapersBReel2020
+LOCAL_OVERRIDES_PACKAGES += YouTubeMusicPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
