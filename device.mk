5a98be306a2cbb89f073a4feb02e29890d0132e7
claxten10 <claxten10@gmail.com>
2026-05-21 17:32:44 +0800

    begonia: Build libmtkperf_client as 64-bit only
    Change-Id: I62021e0dd8a21c88f7e3374e86e71f04529cb143
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: wuxinG90 <2096477910df@gmail.com>

--- device.mk
+++ device.mk
@@ -330,8 +330,8 @@ PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.0.vendor \
     vendor.mediatek.hardware.mtkpower@1.1.vendor \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub \
-    libmtkperf_client_vendor \
-    libmtkperf_client
+    libmtkperf_client_vendor:64 \
+    libmtkperf_client:64
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
