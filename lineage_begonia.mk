424b1246e4ad59609344d6ad649e6f2b7bafaf68
Rabbi~Raihan <rbrabbi789@gmail.com>
2026-08-23 00:50:10 +0600

    begonia: Update some Lunaris flags
    *use aosp gallery 
    *use aosp
    *advance color
    *surface flinger boosting
    *mtk anim fix

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -21,12 +21,17 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Lunaris Specific Flags
 LUNARIS_BUILD_TYPE := UNOFFICIAL
 WITH_GMS := true
+WITH_GMS_COMMS_SUITE := false
+TARGET_SUPORTS_GOOGLE_TELEPHONY := false
+TARGET_USE_GPHOTOS := false
 TARGET_OPTIMIZED_DEXOPT := true
 TARGET_SUPORTS_OMX_SERVICE := true
 TARGET_FORCE_ENABLE_BLUR := true
 TARGET_USES_GSANS := true
 TARGET_SUPPORTS_QUICK_TAP := true
-PRODUCT_NO_CAMERA := true
+USE_ADVANCED_DISPLAY_COLOR := true
+PERF_ANIM_OVERRIDE := true
+SURFACE_FLINGER_BOOST := true
 
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/MiuiCameraLeica/config.mk)
